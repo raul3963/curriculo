b713f86d4cdfbb3fca3cc6ee6178295f3821242a
raul3963 <raulsc396@gmail.com>
2022-11-17 09:19:12 -0300

    added react
    added react to project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="topInfo"></div>
+      <div id="body">
+        <div id="leftInfo"></div>
+        <div id="rightInfo"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
